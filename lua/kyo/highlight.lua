3199c9961d7e36a58ac6245c68a772252ab69aab
kyojin <kyojindev@gmail.com>
2023-01-22 23:39:25 -0400

    feat: switch to catpuccin with a full black background

--- lua/kyo/highlight.lua
+++ lua/kyo/highlight.lua
@@ -1,4 +1,4 @@
-vim.cmd([[colorscheme tokyodark]])
+--vim.cmd([[colorscheme tokyodark]])
 vim.cmd([[set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor50]])
 
 --vim.cmd([[hi Normal guibg=NONE ctermbg=NONE cterm=NONE]])
--- lua/kyo/plug.lua
+++ lua/kyo/plug.lua
@@ -53,7 +53,51 @@ require("lazy").setup({
 	"sainnhe/gruvbox-material",
 	"savq/melange-nvim",
 	"bluz71/vim-moonfly-colors",
-	{ "catppuccin/nvim", name = "catppuccin" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				term_colors = true,
+				transparent_background = false,
+				no_italic = false,
+				no_bold = false,
+				styles = {
+					comments = {},
+					conditionals = {},
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+				},
+				color_overrides = {
+					mocha = {
+						base = "#000000",
+						mantle = "#000000",
+						crust = "#000000",
+					},
+				},
+				highlight_overrides = {
+					mocha = function(C)
+						return {
+							TabLineSel = { bg = C.pink },
+							CmpBorder = { fg = C.surface2 },
+							Pmenu = { bg = C.none },
+							TelescopeBorder = { link = "FloatBorder" },
+						}
+					end,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 	{ "Everblush/nvim", name = "everblush" },
 	"tiagovla/tokyodark.nvim",
 	"kvrohit/mellow.nvim",
