23aec5b3085c6b3a1cc789a13cb4d060aa84384e
kyojin <kyojindev@gmail.com>
2023-01-26 00:49:02 -0400

    feat: remove space as noh since there is a default keybind that does the same

--- lua/kyo/maps.lua
+++ lua/kyo/maps.lua
@@ -14,8 +14,6 @@ keyset("n", "te", ":tabedit<CR>", { silent = true })
 keyset("n", "<S-Tab>", ":tabprev<CR>", { silent = true })
 keyset("n", "<Tab>", ":tabnext<CR>", { silent = true })
 
-keyset("n", "<Space>", ":nohlsearch<CR>", { silent = true })
-
 -- Resize splits
 keyset("n", "<C-w><left>", "<C-w><")
 keyset("n", "<C-w><right>", "<C-w>>")
