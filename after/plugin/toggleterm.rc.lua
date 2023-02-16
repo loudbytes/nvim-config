4fbf903c6ebdc73b3e632309da114678b7068750
kyojin <kyojindev@gmail.com>
2023-02-16 11:56:06 -0400

    feaet: restore lazygit in toggleterm

--- after/plugin/toggleterm.rc.lua
+++ after/plugin/toggleterm.rc.lua
@@ -15,27 +15,27 @@ vim.keymap.set("n", "tt", ":ToggleTerm size=15 direction=horizontal<CR>", { sile
 vim.keymap.set("n", "<Leader>tt", ":ToggleTerm direction=tab<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>tf", ":ToggleTerm direction=float<CR>", { silent = true })
 
---local Terminal = require("toggleterm.terminal").Terminal
---local lazygit = Terminal:new({
---	cmd = "lazygit",
---	dir = "git_dir",
---	direction = "float",
---	float_opts = {
---		border = "double",
---	},
---	close_on_exit = true,
---	hidden = true,
---	on_open = function(term)
---		vim.cmd("startinsert!")
---		vim.keymap.set("n", "q", ":close<CR>", { noremap = true, silent = true, buffer = term.bufnr })
---	end,
---	on_close = function(term)
---		vim.cmd("startinsert!")
---	end,
---})
---
---function _lazygit_toggle()
---	lazygit:toggle()
---end
---
---vim.keymap.set("n", "<leader>g", ":lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	dir = "git_dir",
+	direction = "float",
+	float_opts = {
+		border = "curved",
+	},
+	close_on_exit = true,
+	hidden = true,
+	on_open = function(term)
+		vim.cmd("startinsert!")
+		vim.keymap.set("n", "q", ":close<CR>", { noremap = true, silent = true, buffer = term.bufnr })
+	end,
+	on_close = function(_term)
+		vim.cmd("startinsert!")
+	end,
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.keymap.set("n", "<leader>gt", ":lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
