5fc14dce123754153a4d4659a0ad23fe62a063c6
kyojin <kyojindev@gmail.com>
2023-02-16 11:56:29 -0400

    feat: add luasnip

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -2,8 +2,13 @@ local s, cmp = pcall(require, "cmp")
 if not s then
 	return
 end
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 local lspkind = require("lspkind")
+local luasnip = require("luasnip")
 
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
@@ -11,6 +16,29 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
@@ -20,9 +48,15 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 		{ name = "buffer" },
 		{ name = "nvim_lsp_signature_help" },
+		{ name = "luasnip" },
 	}),
 	formatting = {
 		format = lspkind.cmp_format({ wirth_text = false, maxwidth = 50 }),
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
 	},
 })
 
