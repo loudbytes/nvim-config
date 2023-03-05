b4ea1677dca4e4350a8c50ef0c78376c6a1e7837
kyojin <kyojindev@gmail.com>
2023-03-04 20:00:37 -0400

    fix: fix annoying lspsaga icon

--- after/plugin/lspsaga.rc.lua
+++ after/plugin/lspsaga.rc.lua
@@ -3,7 +3,11 @@ if not s then
 	return
 end
 
-saga.setup({})
+saga.setup({
+	ui = {
+		code_action = "",
+	},
+})
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<cr>", opts)
