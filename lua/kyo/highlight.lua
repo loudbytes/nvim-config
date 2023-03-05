40aa20cdfa5914823ba3e8c54c3d347f87f2014b
kyojin <kyojindev@gmail.com>
2023-03-04 20:01:17 -0400

    feat: add handmade theme

--- lua/kyo/highlight.lua
+++ lua/kyo/highlight.lua
@@ -3,5 +3,4 @@ vim.cmd([[set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor
 
 vim.cmd([[hi Todo guibg=NONE]])
 
---vim.cmd([[hi Normal guibg=NONE ctermbg=NONE cterm=NONE]])
---vim.cmd([[hi EndOfBuffer ctermbg=NONE cterm=NONE]])
+vim.cmd.colorscheme("handmade")
