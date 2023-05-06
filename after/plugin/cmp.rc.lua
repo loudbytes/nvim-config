b3e46738e1d69ce17a4c0e346319438483750543
kyojin <kyojindev@gmail.com>
2023-05-06 17:42:03 -0300

    cmp: remove useless option

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -2,7 +2,8 @@ local s, cmp = pcall(require, "cmp")
 if not s then
 	return
 end
-local has_words_before = function()
+
+local function has_words_before()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
@@ -51,7 +52,7 @@ cmp.setup({
 		{ name = "luasnip" },
 	}),
 	formatting = {
-		format = lspkind.cmp_format({ wirth_text = false, maxwidth = 50 }),
+		format = lspkind.cmp_format({ maxwidth = 50 }),
 	},
 	snippet = {
 		expand = function(args)
