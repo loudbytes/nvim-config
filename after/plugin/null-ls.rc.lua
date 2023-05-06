c6b6b22d6b90465962b49cdbb1c82cff9fc5590f
kyojin <kyojindev@gmail.com>
2023-05-06 17:48:02 -0300

    null-ls: fix config

--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -3,15 +3,29 @@ if not s then
 	return
 end
 
+local augroup = vim.api.nvim_create_autogroup("LspFormatting", {})
+
+local function lsp_formatting(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
 null_ls.setup({
 	sources = {
+		-- Diagnostics
 		null_ls.builtins.diagnostics.selene.with({
 			diagnostics_format = "[selene] #{m}\n(#{c})",
 		}),
-        null_ls.builtins.diagnostics.eslint.with({
+		null_ls.builtins.diagnostics.eslint.with({
 			diagnostics_format = "[eslint] #{m}\n(#{c})",
-        }),
-        null_ls.builtins.formatting.prettier,
+		}),
+
+		-- Formatting
+		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.rustfmt,
@@ -19,14 +33,18 @@ null_ls.setup({
 
 	on_attach = function(client, bufnr)
 		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
+			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = augroup })
 			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup_format,
-				buffer = 0,
+				group = augroup,
+				buffer = bufnr,
 				callback = function()
-                    --vim.lsp.buf.format()
+					lsp_formatting(bufnr)
 				end,
 			})
 		end
 	end,
 })
+
+vim.api.nvim_create_user_command("DisableLspFormatting", function()
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+end, { nargs = 0 })
