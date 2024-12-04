22fbc149d2d4d661f8d434935329a15c5e9826b3
kyojin <kyojindev@gmail.com>
2024-12-04 17:55:35 -0400

    chore: remove unused config

--- after/plugin/asynctasks.rc.lua
+++
@@ -1,6 +0,0 @@
--- TODO: check for asynctask
-vim.g.asyncrun_open = 6
-
-vim.keymap.set("n", "<leader>bb", ":AsyncTask project-build<CR>")
-vim.keymap.set("n", "<leader>r", ":AsyncTask project-run<CR>")
-vim.keymap.set("n", "<leader>br", ":AsyncTask project-build-and-run<CR>")
