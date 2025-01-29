fbca69b732a296153d18580d28e40af8d9d2ccdf
loudbytes <kyojindev@gmail.com>
2025-01-29 09:13:30 -0400

    add custom colorscheme

--- lua/core/highlight.lua
+++ lua/core/highlight.lua
@@ -13,4 +13,28 @@ vim.cmd([[
 ]])
 
 --vim.cmd([[colorscheme gruvbox]])
-vim.cmd([[colorscheme solarized-osaka]])
+--vim.cmd([[colorscheme solarized-osaka]])
+--vim.cmd([[colorscheme handmade]])
+--vim.cmd.colorscheme("gruvbox")
+--vim.cmd.colorscheme("solarized-osaka")
+--vim.cmd.colorscheme("handmade")
+vim.cmd.colorscheme("custom")
+local links = {
+	["@lsp.type.namespace"] = "@namespace",
+	["@lsp.type.type"] = "@type",
+	["@lsp.type.class"] = "@type",
+	["@lsp.type.enum"] = "@type",
+	["@lsp.type.interface"] = "@type",
+	["@lsp.type.struct"] = "@structure",
+	["@lsp.type.parameter"] = "@parameter",
+	["@lsp.type.variable"] = "@variable",
+	["@lsp.type.property"] = "@property",
+	["@lsp.type.enumMember"] = "@constant",
+	["@lsp.type.function"] = "@function",
+	["@lsp.type.method"] = "@method",
+	["@lsp.type.macro"] = "@macro",
+	["@lsp.type.decorator"] = "@function",
+}
+for newgroup, oldgroup in pairs(links) do
+	vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
