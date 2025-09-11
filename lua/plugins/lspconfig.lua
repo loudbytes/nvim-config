4501dc871852c7c8ad84af00b0d66845f60806ad
loudbytes <kyojindev@gmail.com>
2025-09-11 18:16:50 -0300

    add tinymist

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -153,6 +153,11 @@ return {
 				},
 			})
 
+			nvim_lsp.tinymist.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+
 			vim.lsp.handlers["textDocument/publishDiagnostics"] =
 				vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 					underline = true,
