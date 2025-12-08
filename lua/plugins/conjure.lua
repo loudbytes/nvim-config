2b40f811d2027532ed327d2e69ebb968212a994f
loudbytes <kyojindev@gmail.com>
2025-12-08 00:37:05 -0400

    Add fennel support to conjure config

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -1,12 +1,17 @@
 return {
 	{
 		"Olical/conjure",
-		ft = { "janet" },
+		ft = { "fennel", "janet" },
 		lazy = true,
 
 		init = function()
 			vim.g["conjure#filetype#janet"] = "conjure.client.janet.stdio"
+			vim.g["conjure#filetype#fennel"] = "conjure.client.fennel.stdio"
+
 			vim.g["conjure#mapping#doc_word"] = "gK" -- K is already our key for lsp definitions
+
+			-- Prefix conjure commands with ,c
+			--vim.g["conjure#mapping#prefix"] = ",c"
 
 			-- uncomment to get verbose logging (useful when conjure breaks)
 			-- vim.g["conjure#debug"] = true
