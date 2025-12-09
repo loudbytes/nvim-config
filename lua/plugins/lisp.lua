f710f75ab3fefb9a411a701d10a96626b59c7308
loudbytes <kyojindev@gmail.com>
2025-12-09 01:20:49 -0400

    Fix issue with vim-sexp not letting me place my fucking brackets

--- lua/plugins/lisp.lua
+++ lua/plugins/lisp.lua
@@ -3,6 +3,10 @@ return {
 		"guns/vim-sexp",
 		config = function()
 			vim.g.sexp_filetypes = "clojure,scheme,lisp,timl,janet,fennel"
+
+			-- These are handled by nvim-surround
+			vim.g.sexp_enable_insert_mode_mapping = false
+			vim.g.sexp_insert_after_wrap = false
 		end,
 	},
 	"tpope/vim-sexp-mappings-for-regular-people",
