26f3867a01ee72b6a4c949ab3e207d0c2fa7b35e
loudbytes <kyojindev@gmail.com>
2025-12-11 01:23:33 -0400

    Stop render-markdown from highlighting background of headings

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,16 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	opts = {},
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	opts = {
+		heading = {
+			backgrounds = {
+				"NONE",
+				"NONE",
+				"NONE",
+				"NONE",
+				"NONE",
+				"NONE",
+			},
+		},
+	},
 }
