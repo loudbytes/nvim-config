8186b2987168a675ce4fc6fb1f72833ded4e4371
loudbytes <kyojindev@gmail.com>
2025-12-10 22:08:59 -0400

    Issues with lisp plugins...

--- lua/plugins/lisp.lua
+++ lua/plugins/lisp.lua
@@ -1,12 +1,14 @@
 return {
 	{
 		"guns/vim-sexp",
-		config = function()
+		enable = false,
+		init = function()
 			vim.g.sexp_filetypes = "clojure,scheme,lisp,timl,janet,fennel"
 
-			-- These are handled by nvim-surround
-			vim.g.sexp_enable_insert_mode_mapping = false
-			vim.g.sexp_insert_after_wrap = false
+			-- These are neat and all, but they don't let me close orphaned parenthesis.
+			-- It seems the expected behavior is broken, so I'd rather keep them off instead.
+			vim.g["sexp_enable_insert_mode_mappings"] = false
+			vim.g["sexp_insert_after_wrap"] = false
 		end,
 	},
 	"tpope/vim-sexp-mappings-for-regular-people",
